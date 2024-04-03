47344a6cc267b148718498540f775c373a5bbbeb
Saurab Ghimire <sourrab@gmail.com>
2024-04-03 16:52:17 -0500

    one to many association

--- src/main/java/Lab2/Problem2b/OrderLine.java
+++ src/main/java/Lab2/Problem2b/OrderLine.java
@@ -1,4 +1,41 @@
 package Lab2.Problem2b;
 
 public class OrderLine {
+    int orderLineNum;
+    double price;
+    int quantity;
+
+    OrderLine(int orderLineNum, double price, int quantity){
+        this.orderLineNum = orderLineNum;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getOrderLineNum() {
+        return orderLineNum;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double computeTotal(){
+        return price * quantity;
+    }
+
+    public void setOrderLineNum(int orderLineNum) {
+        this.orderLineNum = orderLineNum;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
