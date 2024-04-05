be3b8c9034ad660d8d210ce121714f14b904e4a8
Yogen Pokhrel <yallahproperty@Diks-Yogen.local>
2024-04-05 01:28:47 -0500

    Problem 3 fixes based on the question

--- src/main/java/Lab3/prob3/Admin.java
+++ src/main/java/Lab3/prob3/Admin.java
@@ -6,12 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Admin {
-    List<Property> properties;
-    Admin(Property property){
-        properties = new ArrayList<>();
-        properties.add(property);
-    }
-	public double computeTotalRent() {
+	public static double computeTotalRent(List<Property> properties) {
 		double totalRent = 0;
 		for (Property o : properties) {
 			totalRent += o.computeRent();
@@ -19,11 +14,8 @@ public class Admin {
 		return totalRent;
 	}
 
-    public void addProperty(Property property){
-        properties.add(property);
-    }
 
-	public static List<Property> listPropertiesInCity(Property[] properties, String city) {
+	public static List<Property> listPropertiesInCity(List<Property> properties, String city) {
         List<Property> propertiesInCity = new ArrayList<>();
         for (Property property : properties) {
             if (property.getAddress().getCity().equals(city)) {
@@ -31,15 +23,5 @@ public class Admin {
             }
         }
         return propertiesInCity;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder("Admin { \n\tProperties: [ \n");
-        for(Property b: properties){
-            sb.append("\t\t" + b + "\n");
-        }
-        sb.append("\t\t]\n\t}");
-        return sb.toString();
     }
 }
--- src/main/java/Lab3/prob3/Driver.java
+++ src/main/java/Lab3/prob3/Driver.java
@@ -6,25 +6,30 @@ public class Driver {
 
 	public static void main(String[] args) {
 		Property p1 = new House(new Address("1000 N 4th street", "Fairfield", "IA",  "52557"), 1200);
-		Admin a1 = new Admin(p1);
+		Landlord l1 = new Landlord(p1);
 		Address[] addresses = {
 				new Address("111 Main", "Fairfield", "IA", "52556"),
-				new Address("200 Forest Ave", "Fairfield", "IA", "52556"),
+				new Address("200 Forest Ave", "Chicago", "IA", "52556"),
 			    new Address("10 N. 4th St.", "Fairfield", "IA", "52556")
 		};
-		Property[] properties = { 
-				new House(addresses[0], 1200.0), 
-				new Condo(addresses[1], 2), 
-				new Trailer(addresses[2]) 
+		Property[] properties = {
+				new House(addresses[0], 1200.0),
+				new Condo(addresses[1], 2),
+				new House(addresses[1], 1400),
+				new Trailer(addresses[2])
 		};
 
 		for(Property p: properties){
-			a1.addProperty(p);
+			l1.addProperty(p);
 		}
 
-		double totalRent = a1.computeTotalRent();
+		double totalRent = Admin.computeTotalRent(l1.getProperties());
+		List<Property> allPropertiesInCity = Admin.listPropertiesInCity(l1.getProperties(), "Chicago");
 		System.out.println("Total rent is " + totalRent);
 		System.out.println("All admin properties are");
-		System.out.println(a1);
+		System.out.println(l1);
+
+		System.out.println("All properties in Chicago city are");
+		System.out.println(allPropertiesInCity);
 	}
 }
