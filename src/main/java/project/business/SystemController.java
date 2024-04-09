4f42e58f99f3ae13aa9c47646a7aed50a1b02a5b
Yogen Pokhrel <yallahproperty@Diks-Yogen.local>
2024-04-09 15:33:16 -0500

    Project Setup

---
+++ src/main/java/project/business/SystemController.java
@@ -0,0 +1,45 @@
+package project.business;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import project.dataaccess.Auth;
+import project.dataaccess.DataAccess;
+import project.dataaccess.DataAccessFacade;
+import project.dataaccess.User;
+
+public class SystemController implements ControllerInterface {
+	public static Auth currentAuth = null;
+	
+	public void login(String id, String password) throws LoginException {
+		DataAccess da = new DataAccessFacade();
+		HashMap<String, User> map = da.readUserMap();
+		if(!map.containsKey(id)) {
+			throw new LoginException("ID " + id + " not found");
+		}
+		String passwordFound = map.get(id).getPassword();
+		if(!passwordFound.equals(password)) {
+			throw new LoginException("Password incorrect");
+		}
+		currentAuth = map.get(id).getAuthorization();
+		
+	}
+	@Override
+	public List<String> allMemberIds() {
+		DataAccess da = new DataAccessFacade();
+		List<String> retval = new ArrayList<>();
+		retval.addAll(da.readMemberMap().keySet());
+		return retval;
+	}
+	
+	@Override
+	public List<String> allBookIds() {
+		DataAccess da = new DataAccessFacade();
+		List<String> retval = new ArrayList<>();
+		retval.addAll(da.readBooksMap().keySet());
+		return retval;
+	}
+	
+	
+}
