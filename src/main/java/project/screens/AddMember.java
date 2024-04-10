0776197c8dc2f9257cd7b2cc0bd70b74d6966299
Yogen Pokhrel <yallahproperty@Diks-Yogen.local>
2024-04-10 13:43:00 -0500

    Add Member form

---
+++ src/main/java/project/screens/AddMember.java
@@ -0,0 +1,19 @@
+package project.screens;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AddMember {
+    private JButton button1;
+    private JPanel panel1;
+
+    public AddMember() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
