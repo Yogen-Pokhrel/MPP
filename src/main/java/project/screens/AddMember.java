8b7c54c1b4d7a5c4fc74b5b047a3621df34c5f88
Saurab Ghimire <sourrab@gmail.com>
2024-04-12 13:45:38 -0500

    add validation to AddMember

--- src/main/java/project/screens/AddMember.java
+++ src/main/java/project/screens/AddMember.java
@@ -5,6 +5,8 @@ import project.business.Book;
 import project.business.LibraryMember;
 import project.dataaccess.DataAccess;
 import project.dataaccess.DataAccessFacade;
+import project.project.utils.validation.DialogUtils;
+import project.project.utils.validation.ValidationUtils;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -22,9 +24,22 @@ public class AddMember extends Routes implements Component{
     private JTextField city;
     private JTextField state;
     private JTextField zipCode;
+    private JLabel fNameLabel;
+    private JLabel lNameLabel;
+    private JLabel phoneNumberLabel;
+    private JLabel bioLabel;
+    private JLabel addressLabel;
+    private JLabel streetLabel;
+    private JLabel cityLabel;
+    private JLabel stateLabel;
+    private JLabel zipCodeLabel;
+    private StringBuilder validationMessage;
+
     private static AddMember instance;
 
     private AddMember() {
+        validationMessage= new StringBuilder();
+
         saveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -32,6 +47,12 @@ public class AddMember extends Routes implements Component{
             }
         });
         saveButton.addActionListener(e -> {
+            validationMessage.setLength(0);
+            validateInput();
+            if(!validationMessage.isEmpty()){
+                DialogUtils.showValidationMessage(validationMessage.toString());
+                return;
+            }
             LibraryMember libraryMember = new LibraryMember(
                     ("M-" + java.lang.System.currentTimeMillis()),
                     fName.getText(),
@@ -72,5 +93,22 @@ public class AddMember extends Routes implements Component{
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
+    }
+
+    void  validateInput(){
+        validateEmptyFields();
+        ValidationUtils.validatePhoneNumber(phone, phoneNumberLabel, validationMessage);
+        ValidationUtils.validateZipCode(zipCode, zipCodeLabel, validationMessage);
+    }
+
+    void validateEmptyFields(){
+        ValidationUtils.validateField(fName, fNameLabel, validationMessage);
+        ValidationUtils.validateField(lName, lNameLabel, validationMessage);
+        ValidationUtils.validateField(phone, phoneNumberLabel, validationMessage);
+        ValidationUtils.validateField(bio, bioLabel, validationMessage);
+        ValidationUtils.validateField(street, streetLabel, validationMessage);
+        ValidationUtils.validateField(city, cityLabel, validationMessage);
+        ValidationUtils.validateField(state, stateLabel, validationMessage);
+        ValidationUtils.validateField(zipCode, zipCodeLabel, validationMessage);
     }
 }
