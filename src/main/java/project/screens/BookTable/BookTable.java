e5133cedb622116f61b471c21d8fec8eef82b264
anubra266 <anubra266@gmail.com>
2024-04-12 12:48:35 -0500

    Add book copy

---
+++ src/main/java/project/screens/BookTable/BookTable.java
@@ -0,0 +1,56 @@
+package project.screens.BookTable;
+
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
+
+import project.business.Book;
+
+public class BookTable extends JTable {
+
+    public BookTable(Object[][] rowData, Object[] columnNames) {
+
+        super(new TableModel(rowData, columnNames));
+        getTableHeader().setFont(new Font("SansSerif", Font.BOLD, 16));
+
+        TableColumnModel columnModel = getColumnModel();
+
+        columnModel.getColumn(0).setPreferredWidth(30);
+        columnModel.getColumn(1).setPreferredWidth(150);
+        columnModel.getColumn(2).setPreferredWidth(10);
+        columnModel.getColumn(3).setPreferredWidth(150);
+
+        columnModel.getColumn(4).setCellRenderer(new ButtonRenderer());
+
+        addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                int row = rowAtPoint(evt.getPoint());
+                int col = columnAtPoint(evt.getPoint());
+                if (row >= 0 && col == 4) {
+                    Book book = (Book) getValueAt(row, col);
+                    book.addCopy();
+                }
+            }
+        });
+    }
+
+    public class ButtonRenderer extends JButton implements TableCellRenderer {
+
+        public ButtonRenderer() {
+            setOpaque(true);
+        }
+
+        @Override
+        public java.awt.Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+                boolean hasFocus,
+                int row, int column) {
+            setText("Add Copy");
+            return this;
+        }
+
+    }
+}
---
+++ src/main/java/project/screens/BookTable/TableModel.java
@@ -0,0 +1,16 @@
+package project.screens.BookTable;
+
+import javax.swing.table.DefaultTableModel;
+
+public class TableModel extends DefaultTableModel {
+
+    public TableModel(Object[][] data, Object[] columnNames) {
+        super(data, columnNames);
+    }
+
+    @Override
+    public boolean isCellEditable(int row, int column) {
+        return false;
+    }
+
+}
