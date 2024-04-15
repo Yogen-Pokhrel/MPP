b45e0c1b59efe128e872bfa9fe1d04cebaf329e9
anubra266 <anubra266@gmail.com>
2024-04-15 14:08:31 -0500

    Lab7 prob 3 solution

---
+++ src/main/java/Lab7/prob3/ForEachExample.java
@@ -0,0 +1,23 @@
+package Lab7.prob3;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ForEachExample {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("Hello there", "Goodbye", "Back soon",
+                "Away", "On Vacation", "Everywhere you want to be");
+
+        list.forEach(consumer);
+
+    }
+
+    static Consumer<String> consumer = new Consumer<String>() {
+        @Override
+        public void accept(String s) {
+            System.out.println(s.toUpperCase());
+        }
+    };
+
+}
