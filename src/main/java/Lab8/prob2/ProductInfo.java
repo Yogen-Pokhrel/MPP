96b4fabeb218c82f08b3eb91223aaea7910794a8
anubra266 <anubra266@gmail.com>
2024-04-16 15:17:59 -0500

    Prob 2

--- src/main/java/Lab8/prob2/ProductInfo.java
+++ src/main/java/Lab8/prob2/ProductInfo.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import Lab8.prob2.prob2a.ProductPriceComparator;
+import Lab8.prob2.prob2b.ProductTitleComparator;
+
 public class ProductInfo {
 
 
