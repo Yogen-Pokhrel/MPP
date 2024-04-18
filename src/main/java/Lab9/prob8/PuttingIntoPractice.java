79469c7b402f09fe52f18f77029d032a76ec6221
Saurab Ghimire <sourrab@gmail.com>
2024-04-18 17:14:50 -0500

    lab9 prob8

--- src/main/java/Lab9/prob8/PuttingIntoPractice.java
+++ src/main/java/Lab9/prob8/PuttingIntoPractice.java
@@ -1,5 +1,10 @@
 package Lab9.prob8;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
 
@@ -18,28 +23,32 @@ public class PuttingIntoPractice{
             new Transaction(mario, 2012, 700),
             new Transaction(alan, 2012, 950)
         );	
-        
-        
+
         // Query 1: Find all transactions from year 2011 and sort them by value (small to high).
-
-
+        System.out.println("Query 1: Find all transactions from year 2011 and sort them by value (small to high)");
+        transactions.stream().filter(x  -> x.getYear() == 2011).sorted(Comparator.comparing(Transaction::getValue)).forEach(System.out::println);
         
         // Query 2: What are all the unique cities where the traders work?
-        
+        System.out.println("Query 2: What are all the unique cities where the traders work?");
+        transactions.stream().map(Transaction::getTrader).map(Trader::getCity).distinct().forEach(System.out::println);
 
         // Query 3: Find all traders from Cambridge and sort them by name.
-        
-   
+        System.out.println("Query 3: Find all traders from Cambridge and sort them by name.");
+        transactions.stream().map(Transaction::getTrader).filter(x -> x.getCity().equals("Cambridge")).distinct().sorted(Comparator.comparing(Trader::getName)).forEach(System.out::println);
         
         // Query 4: Return a string of all traders names sorted alphabetically.
-        
+        System.out.println("Query 4: Return a string of all traders names sorted alphabetically.");
+        transactions.stream().map(Transaction::getTrader).sorted(Comparator.comparing(Trader::getName)).distinct().forEach(System.out::println);
 
         // Query 5: Are there any trader based in Milan?
-        
+        boolean isPresentInMilan = transactions.stream().map(Transaction::getTrader).filter(x -> x.getCity().equals("Milan")).findAny().isPresent();
+        System.out.println("Query 5: Are there any trader based in Milan?\n" + isPresentInMilan);
    
-     // Query 6: Update all transactions so that the traders from Milan are set to Cambridge.
-        
-        
+        // Query 6: Update all transactions so that the traders from Milan are set to Cambridge.
+//        transactions.stream().map(Transaction::getTrader).filter(x -> x.getCity().equals("Milan")).reduce().forEach(System.out::println);
+
         // Query 7: What's the highest value in all the transactions?
+        Optional<Integer> maxValue = transactions.stream().map(Transaction::getValue).max(Integer::compare);
+        System.out.println("Query 7: What's the highest value in all the transactions?\n" + maxValue.get());
     }
 }
