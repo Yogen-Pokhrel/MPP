5e6e73ce6396705d97700fb1ca588aca67f6f995
Saurab Ghimire <sourrab@gmail.com>
2024-04-17 21:08:04 -0500

    lab8 prob4C

--- src/main/java/Lab8/prob4/probC.java
+++ src/main/java/Lab8/prob4/probC.java
@@ -1,4 +1,16 @@
 package Lab8.prob4;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//String[] names = {"Alexis", "Tim", "Kyleen", "Kristy"};
+//b. Convert the sorted names array into List.
 public class probC {
+    public static void main(String[] args) {
+        String[] names = {"Alexis", "Tim", "Kyleen", "Kristy"};
+        Arrays.sort(names, String::compareTo);
+        List<String> namesList = Arrays.stream(names).collect(Collectors.toList());
+        namesList.forEach(System.out::println);
+    }
 }
