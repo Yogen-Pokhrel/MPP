367a94a3b759cb235c692469185612b3ae18a64c
anubra266 <anubra266@gmail.com>
2024-04-23 03:31:27 -0500

    lab10/prob1

--- src/main/java/Lab10/prob1/GroupUtil.java
+++ src/main/java/Lab10/prob1/GroupUtil.java
@@ -1,19 +1,28 @@
 package Lab10.prob1;
+
 import java.util.*;
 
 public class GroupUtil {
-	//Fix this code
-//	public static Group<?> copy(Group<?> group) {
-//		List<?> elements = group.getElements();
-//		Group<?> grp = new Group<?>(group.getSpecialElement(), elements);
-//		return grp;
-//	}
-	
-	//Test using this main method
+	public static <T> Group<T> copy(Group<T> group) {
+		List<T> elements = group.getElements();
+		Group<T> grp = new Group<T>(group.getSpecialElement(), elements);
+		return grp;
+	}
+
+	// Test using this main method
 	public static void main(String[] args) {
-		List<Integer> list = Arrays.asList(0,1,2,3,4);
-		Group<Integer> group = new Group<>(0, list);
-		System.out.println(group);
-		//System.out.println(GroupUtil.copy(group));
+		List<Integer> elements1 = Arrays.asList(1, 2, 3, 4);
+		Group<Integer> group1 = new Group<>(1, elements1);
+		System.out.println("Original Group: " + group1);
+
+		Group<Integer> copy1 = copy(group1);
+		System.out.println("Copied Group: " + copy1);
+
+		List<String> elements2 = Arrays.asList("C++", "C#", "Java", "Kotlin");
+		Group<String> group2 = new Group<>("Java", elements2);
+		System.out.println("Original Group: " + group2);
+
+		Group<String> copy2 = copy(group2);
+		System.out.println("Copied Group: " + copy2);
 	}
 }
